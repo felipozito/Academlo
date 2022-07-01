6a06dd48de6952954acd23b5273c353647a40d54
felipozito <felipozito@hotmail.com>
2022-07-01 18:39:28 -0500

    50/100 Portfolio Academlo

--- Portfolio Academlo/Portfolio/script.js
+++ Portfolio Academlo/Portfolio/script.js
@@ -1,8 +1,28 @@
 const icon = document.querySelector(".nav_ico img");
 const menu = document.querySelector(".nav_item");
+const text = document.querySelector("h1");
+const number_skills = document.querySelectorAll(".skills_item h2");
+//------------navbar
 icon.addEventListener("click", () => {
       menu.classList.toggle("active");
 });
 window.addEventListener("scroll", () => {
       menu.classList.remove("active");
+});
+//------------Effect type
+const str = text.innerHTML;
+text.innerHTML = " ";
+let i = 0;
+
+function typing() {
+      if (i < str.length) {
+            text.innerHTML += str.charAt(i);
+            i++;
+            setTimeout(typing, 200);
+      }
+}
+setTimeout(typing, 200);
+//---------------Skill
+number_skills.forEach((item) => {
+      console.log(item.textContent);
 });
