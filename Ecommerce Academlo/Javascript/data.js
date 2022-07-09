d65d05a88dcff74ec184953a461e6831936d51b6
felipozito <felipozito@hotmail.com>
2022-07-09 09:31:45 -0500

    Avance 30% Ecommerce

--- Ecommerce Academlo/Javascript/data.js
+++ Ecommerce Academlo/Javascript/data.js
@@ -1,23 +1,5 @@
-products = [
-      {
-            name: "Hoddies",
-            price: 35,
-            stock: 20,
-            collection: 2022,
-            state: "free",
-      },
-      {
-            name: "Shirts",
-            price: 20,
-            stock: 15,
-            collection: 2022,
-            state: "free",
-      },
-      {
-            name: Sweartshirt,
-            price: 40,
-            stock: 18,
-            collection: 2022,
-            state: "free",
-      },
+export const products = [
+      { id: 1, name: "Hoddies", price: 35, stock: 20, collection: 2022, state: "free", type: "hoddies" },
+      { id: 2, name: "Shirts", price: 20, stock: 15, collection: 2022, state: "free", type: "shirt" },
+      { id: 3, name: "Sweartshirt", price: 40, stock: 18, collection: 2022, state: "free", type: "sweartshirt" },
 ];
--- Ecommerce Academlo/Javascript/main.js
+++ Ecommerce Academlo/Javascript/main.js
@@ -1,8 +1,14 @@
+import { data } from "./data";
 iconTheme = document.querySelectorAll(".navbar_items i");
+navbar = document.querySelector(".navbar");
+navbar_list = document.querySelector(".navbar_label");
 shoppingCart = document.querySelector(".shopping_cart");
 
+console.log(data);
 window.addEventListener("click", (e) => {
       const event = e.target.classList.value;
+      const type = e.target.dataset.type;
+      console.log(type);
       if (event.includes("bx bx-moon") || event.includes("bx bx-sun")) {
             darkmode(event);
       }
@@ -12,11 +18,20 @@ window.addEventListener("click", (e) => {
       if (event.includes("shopping_close")) {
             closeCart();
       }
+      if (event.includes("bx bx-menu")) {
+            showMenu();
+      }
 });
 
+window.addEventListener("scroll", () => {
+      if (window.scrollY == 0) {
+            navbar.classList.remove("sticky");
+      } else {
+            navbar.classList.add("sticky");
+      }
+});
 function darkmode(event) {
       if (event.includes("bx bx-moon")) {
-            console.log(document.body);
             iconTheme[1].classList.replace("bx-moon", "bx-sun");
             document.body.classList.add("darkMode");
       } else {
@@ -29,4 +44,15 @@ function openCart() {
 }
 function closeCart() {
       document.querySelector(".shopping_cart").classList.remove("open_cart");
+}
+function showMenu() {
+      navbar_list.classList.toggle("show_nav");
+      if (navbar_list.classList.value.includes("show_nav")) {
+            navbar.classList.add("sticky");
+      } else {
+            navbar.classList.remove("sticky");
+      }
+}
+function filterItems() {
+      console.log(item);
 }
