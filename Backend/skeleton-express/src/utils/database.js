aaa7b3e5637d745c7e958645bc4037817d4a6f5e
felipozito <felipozito@hotmail.com>
2022-10-17 19:54:21 -0500

    Skeleton modificated v1

--- Backend/skeleton-express/src/utils/database.js
+++ Backend/skeleton-express/src/utils/database.js
@@ -1,12 +1,12 @@
-const { Sequelize } = require('sequelize')
-const config = require('../config')
+const { Sequelize } = require("sequelize");
+const config = require("../config");
 
 const db = new Sequelize({
-    dialect: 'postgres',
-    host: config.db.host, //? Variable de entorno del host
-    username: config.db.username, //? Variable de entorno del usuario
-    password: config.db.password, //? Variable de entorno de la contraseña
-    database: config.db.dbName //? Variable de entorno de la base de datos
-})
+      dialect: "postgres",
+      host: config.db.host,
+      username: config.db.username,
+      password: config.db.password,
+      database: config.db.dbName,
+});
 
-module.exports = db
+module.exports = db;
