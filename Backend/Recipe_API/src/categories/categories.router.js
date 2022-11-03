ca631ee6b021ef43aad4c7a87f63c41454d33487
felipozito <felipozito@hotmail.com>
2022-11-03 12:35:47 -0500

    Recipes

--- Backend/Recipe_API/src/categories/categories.router.js
+++ Backend/Recipe_API/src/categories/categories.router.js
@@ -1,17 +1,27 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const passport = require("passport");
 
-const categoryServices = require('./categories.services')
+const categoryServices = require("./categories.services");
+const adminMiddleware = require("../middlewares/role.middleware");
 
-//? / 
+require("../middlewares/auth.middleware")(passport);
+//? /
 //? /:id
 
+router.route("/")
+  .get(categoryServices.getAllCategories)
+  .post(
+    passport.authenticate("jwt", { session: false }),
+    adminMiddleware,
+    categoryServices.postCategory
+  );
 
-router.route('/')
-    .get(categoryServices.getAllCategories)
-    .post(categoryServices.postCategory) //TODO hacerla protegida por administrador
+router.route("/:id")
+  .get(categoryServices.getCategoryById)
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    adminMiddleware,
+    categoryServices.deleteCategory
+  ); 
 
-router.route('/:id')
-    .get(categoryServices.getCategoryById)
-    .delete(categoryServices.deleteCategory) //TODO hacerla protegida por administrador
-
-module.exports = router
+module.exports = router;
